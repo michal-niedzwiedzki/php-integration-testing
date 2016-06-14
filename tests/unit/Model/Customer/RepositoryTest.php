9404bbc1c93a9960ba252bd3629327c1bcfea30e
Michał Rudnicki <michal.rudnicki@epsi.pl>
2016-06-14 10:14:20 +0200

    cleanup

--- tests/unit/Model/Customer/RepositoryTest.php
+++ tests/unit/Model/Customer/RepositoryTest.php
@@ -15,19 +15,19 @@ class RepositoryTest extends \PHPUnit_Framework_TestCase {
 	 */
 	public function test_fetchById() {
 
-		// create pdo instance
-		// ...
-
-		// instantiate repository and fetch actual value
-		// ...
-
 		// expected value
-		$record = [
+		$expected = [
 			"id" => 1,
 			"name" => "John Doe",
 			"email" => "johndoe@gmail.com",
 			"password" => "",
 		];
+
+		// create pdo instance
+		// ...
+
+		// instantiate repository and fetch actual value
+		// ...
 
 		// test actual against expected
 		// ...
